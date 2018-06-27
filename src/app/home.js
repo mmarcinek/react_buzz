abfbd7f7c27abcb178aae1f0060742e89b02ea23
Michael Marcinek <michaelmarcinek@gmail.com>
2018-06-27 14:49:11 -0400

    makes increment and decrement button enabled

--- src/app/home.js
+++ src/app/home.js
@@ -1,44 +1,13 @@
 import React from 'react'
 
 import Response from './response'
-import TextField from '@material-ui/core/TextField'
-
-// import { withStyles } from '@material-ui/core/styles'
-import Button from '@material-ui/core/Button'
 
 export default class Home extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      endingValue: null
-    }
-  }
-
-  fizzTheBuzz() {
-    let count = document.getElementById('countToThis').value
-
-    this.setState({endingValue: count})
-  } 
-
   render() {
-    if(this.state.endingValue) {
-      return <Response
-              endingValue={this.state.endingValue}
-              onClear={() => this.setState({endingValue: null})}
-              />
-    } else {
-      return(
-        <div>
-          <h1>Please enter a value to count to</h1>
-          <TextField
-            id='countToThis'
-            margin='normal'
-          />
-          <Button variant='outlined' color='primary' onClick={this.fizzTheBuzz.bind(this)}>
-            Let's do this!
-          </Button>
-        </div>
-      )
-    }
+    return( 
+      <Response
+        onClear={() => this.setState({endingValue: null})}
+      />
+    )
   }
 }
--- src/app/response.js
+++ src/app/response.js
@@ -8,50 +8,53 @@ export default class Response extends React.Component {
     super(props)
     
     this.state = {
-      endingValue: Number(this.props.endingValue),
-      count: 1
+      count: 0
     }
   }
 
   componentDidMount(){
-    const newState = {}
-
-    this.interval = setInterval(() => {
-      const {endingValue, count} = this.state      
-      if(endingValue >= count){
-
-        // assigning text based on count in ternary made pretty
-        newState.text = count % 15 === 0 ? 'FizzBuzz' : 
-                        count %  5 === 0 ? 'Buzz'     : 
-                        count %  3 === 0 ? 'Fizz'     : 
-                        count
-
-        // iterate count state
-        newState.count = count + 1
-
-        if (endingValue === count){
-          newState.showClear = true
-        }
-      }
-
-      this.setState(newState)
-    }, 1000)
+    this.updateComponent()
   }
 
-  componentWillUnmount() {
-    clearInterval(this.interval)
+  updateComponent(i){
+    const newState = {}
+
+    const {count} = this.state
+   
+    newState.count = 
+      i === 'increase' ? count + 1 :
+      i === 'decrease' ? count - 1 :
+      count
+
+    /* assigning text based on count in ternary made pretty */
+    newState.text = 
+      newState.count % 15 === 0 ? 'FizzBuzz' : 
+      newState.count %  5 === 0 ? 'Buzz'     : 
+      newState.count %  3 === 0 ? 'Fizz'     :
+      ''
+    this.setState(newState)
+  }
+
+  onIncrease(){
+    this.updateComponent('increase')
+  }
+
+  onDecrease(){
+    this.updateComponent('decrease')
   }
 
   render(){
-    const { text, showClear } = this.state
+    const { text, count } = this.state
     return (
       <div>
-        <h1>{ text }</h1>
-        { showClear && 
-          <Button variant='outlined' color='primary' onClick={ this.props.onClear }>
-            Clear!
-          </Button>
-        }
+        <h2>{ count }</h2>        
+        <h2>{ text }</h2>
+        <Button variant='outlined' color='primary' onClick={this.onIncrease.bind(this)}>
+          +
+        </Button>
+        <Button variant='outlined' color='primary' onClick={ this.onDecrease.bind(this) }>
+          -
+        </Button>
       </div>
     )
   }
