ec393d8ab33ebba84ec4d19548b7e4427e9ccb1e
Michael Marcinek <michaelmarcinek@gmail.com>
2018-06-27 17:45:24 -0400

    move state to parent, and add particle library

--- src/app/home.js
+++ src/app/home.js
@@ -2,12 +2,60 @@ import React from 'react'
 
 import Response from './response'
 
+import Particles from 'react-particles-js';
+import particles from '../particles.json'
+
 export default class Home extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      count: 1
+    }
+
+  }
+
+  componentDidMount() {
+    this.updateComponent()
+  }
+
+  updateComponent(i) {
+    const newState = {}
+
+    const { count } = this.state
+
+    newState.count =
+      i === 'increase' ? count + 1 :
+      i === 'decrease' ? count - 1 :
+      count
+    
+    /* assigning text based on count in ternary made pretty */
+    newState.text =
+      newState.count % 15 === 0 ? 'FizzBuzz' :
+      newState.count %  5 === 0 ? 'Buzz'     :
+      newState.count %  3 === 0 ? 'Fizz'     :
+      ''
+    
+    this.setState(newState)
+  }
+
   render() {
-    return( 
-      <Response
-        onClear={() => this.setState({endingValue: null})}
-      />
+    const { text, count } = this.state
+    
+    return (
+      <div>
+        <Particles 
+          params={ particles } 
+          style={{
+            backgroundImage: `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' version='1.1' height='50px' width='120px'><text x='0' y='15' fill='red' font-size='20'>${text}</text></svg>")`
+          }}
+        />    
+        <Response 
+          count={count}
+          onIncrease={() => this.updateComponent('increase')}
+          onDecrease={() => this.updateComponent('decrease')}
+        />
+      </div>
     )
   }
 }
--- src/app/response.js
+++ src/app/response.js
@@ -4,55 +4,15 @@ import React from 'react'
 import Button from '@material-ui/core/Button'
 
 export default class Response extends React.Component {
-  constructor(props){
-    super(props)
-    
-    this.state = {
-      count: 0
-    }
-  }
-
-  componentDidMount(){
-    this.updateComponent()
-  }
-
-  updateComponent(i){
-    const newState = {}
-
-    const {count} = this.state
-   
-    newState.count = 
-      i === 'increase' ? count + 1 :
-      i === 'decrease' ? count - 1 :
-      count
-
-    /* assigning text based on count in ternary made pretty */
-    newState.text = 
-      newState.count % 15 === 0 ? 'FizzBuzz' : 
-      newState.count %  5 === 0 ? 'Buzz'     : 
-      newState.count %  3 === 0 ? 'Fizz'     :
-      ''
-    this.setState(newState)
-  }
-
-  onIncrease(){
-    this.updateComponent('increase')
-  }
-
-  onDecrease(){
-    this.updateComponent('decrease')
-  }
-
-  render(){
-    const { text, count } = this.state
+  render() {
+    const { count, onDecrease, onIncrease } = this.props
     return (
-      <div>    
+      <div>            
         <h2>{count}</h2>
-        <h2>{ text }</h2>
-        <Button variant='outlined' color='primary' onClick={this.onIncrease.bind(this)}>
+        <Button variant='outlined' color='primary' onClick={onIncrease}>
           +
         </Button>
-        <Button variant='outlined' color='primary' onClick={ this.onDecrease.bind(this) }>
+        <Button variant='outlined' color='primary' onClick={onDecrease}>
           -
         </Button>
       </div>
