958c904fcde04efeca9b04edde6502067ca164a3
Michael Marcinek <michaelmarcinek@gmail.com>
2018-06-26 20:49:02 -0400

    clear withStyle temporarily

--- src/app/home.js
+++ src/app/home.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Response from './response'
 import TextField from '@material-ui/core/TextField'
 
-import { withStyles } from '@material-ui/core/styles'
+// import { withStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
 
 export default class Home extends React.Component {
--- src/app/response.js
+++ src/app/response.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { withStyles } from '@material-ui/core/styles'
+// import { withStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
 
 export default class Response extends React.Component {
