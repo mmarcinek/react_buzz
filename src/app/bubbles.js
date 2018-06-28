62c3ec969a000883d571841c460258f6665fda49
Michael Marcinek <michaelmarcinek@gmail.com>
2018-06-27 22:04:09 -0400

    remove background image from canvas

--- src/app/bubbles.js
+++ src/app/bubbles.js
@@ -12,7 +12,6 @@ export default class Bubbles extends React.Component {
         canvasClassName='particleCanvas'
         params={ particles } 
         style={{
-          backgroundImage: `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' version='1.1' height='50px' width='120px'><text x='0' y='15' fill='red' font-size='20'>${text}</text></svg>")`,
           position: 'absolute',
           width: '100%',
           height: '100%',
