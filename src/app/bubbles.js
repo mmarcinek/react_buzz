6f7602f01d19030b3118964391d38da47354af42
Michael Marcinek <michaelmarcinek@gmail.com>
2018-06-27 22:02:33 -0400

    fixes some css

--- src/app/bubbles.js
+++ src/app/bubbles.js
@@ -5,23 +5,6 @@ import particles from '../particles.json'
 
 export default class Bubbles extends React.Component {
   
-  // componentWillReceiveProps(newProps){
-  //       // const { text } = this.props
-  //   if(newProps.text){ 
-  //     const newHtml = `<p>${newProps.text}</p>`
-  //     let canvas = document.getElementsByClassName('particleCanvas')[0]
-  //     console.log(canvas)
-  //     canvas.innerHtml = newHtml
-  //     console.log(canvas)
-  //     // const ctx = canvas[0].getContext('2d')
-      
-  //     // ctx.font = '30px'
-  //     // ctx.fillText(newProps.text, 10, 50)
-  //     // console.log(ctx)
-      
-  //   }
-  // }
-
   render() {
     const { text } = this.props
 
@@ -32,9 +15,9 @@ export default class Bubbles extends React.Component {
         style={{
           backgroundImage: `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' version='1.1' height='50px' width='120px'><text x='0' y='15' fill='red' font-size='20'>${text}</text></svg>")`,
           position: 'absolute',
-          width: 100 + '%',
-          height: 100 + '%',
-          zIndex: -1,
+          width: '100%',
+          height: '100%',
+          zIndex: -2,
         }}
       />    
     )
