1f875ecae354602562b6baf3ed57e84b84df5c10
Michael Marcinek <michaelmarcinek@gmail.com>
2018-06-28 10:47:15 -0400

    save fragment

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
 import App from './app/app';
 import Bubbles from './components/bubbles'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<div>
-      <App />
-      <Bubbles />
-</div>, document.getElementById('root'));
+ReactDOM.render(<React.Fragment>
+	<App />
+	<Bubbles />
+</React.Fragment>, document.getElementById('root'));
 registerServiceWorker();
