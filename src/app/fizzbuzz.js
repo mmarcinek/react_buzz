2874d89213e51bc1edf7a80b82255c5ed6bae1f2
Michael Marcinek <michaelmarcinek@gmail.com>
2018-06-28 13:17:20 -0400

    change HEX value to rgb, chrome depication of # in data uris "

--- src/app/fizzbuzz.js
+++ src/app/fizzbuzz.js
@@ -6,7 +6,7 @@ export default class FizzBuzz extends React.Component{
     return(
       <canvas id="fbCanvas" 
         style={{
-          backgroundImage: `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' version='1.1' height='100px' width='370px'><text x='0' y='60' fill='rgb(44,46,65)' stroke='#2C2E3F' font-size='70' font-family='Roboto'>${text}</text></svg>")`,
+          backgroundImage: `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' version='1.1' height='100px' width='370px'><text x='0' y='60' fill='rgb(44,46,65)' stroke='rgb(44, 46, 63)' font-size='70' font-family='Roboto'>${text}</text></svg>")`,
           backgroundRepeat: 'round',
           position: 'absolute',
           height: '100%',
